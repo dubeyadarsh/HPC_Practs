5541eb35f90515d636dabdc0d3f7b2219e90dcff
Adarsh Dubey <75479397+dubeyadarsh@users.noreply.github.com>
2023-04-23 23:16:49 +0530

    Update area_circle_omp.c

--- area_circle_omp.c
+++ area_circle_omp.c
@@ -1,31 +1,32 @@
 //**Aim :: Area of circle using omp**
 #include <stdio.h>
 #include <omp.h>
-Int main(int argc, char *argv[])
-{ Double radius = 5.0;
-    Double area = 0.0;
-    Double pi = 3.14159;
-    Int num_threads = 4;
+int main(int argc, char *argv[])
+{
+    double radius = 5.0;
+    double area = 0.0;
+   double pi = 3.14159;
+    int num_threads = 4;
 #pragma omp parallel num_threads(num_threads)
     {
-        Int tid = omp_get_thread_num();
-        Double local_area = 0.0;
+        int tid = omp_get_thread_num();
+        double local_area = 0.0;
  #pragma omp for
-        For (int I = 0; I < 10000000; i++) {
-            Double x = (double)rand() / RAND_MAX;
-            Double y = (double)rand() / RAND_MAX;
-            If (x*x + y*y <= radius*radius) {
-                Local_area += 1.0;
+        for (int i = 0; i < 10000000; i++) {
+            double x = (double)rand() / RAND_MAX;
+            double y = (double)rand() / RAND_MAX;
+            if (x*x + y*y <= radius*radius) {
+                local_area += 1.0;
             }
         }
 #pragma omp critical
         {
-            Area += local_area;
+            area += local_area;
         }
     }
-  Area = 4.0 * radius * radius * (area / 10000000.0) / num_threads;
-    Printf(“Area of circle with radius %f is %f\n”, radius, area);
-Return 0;
+  area = 4.0 * radius * radius * (area / 10000000.0) / num_threads;
+    printf(“Area of circle with radius %f is %f\n”, radius, area);
+return 0;
 }
 
 //OUTPUT- 
